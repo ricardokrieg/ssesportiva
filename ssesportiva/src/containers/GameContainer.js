c9319a07d3341510545369ac842e3a60004d1543
Ricardo Franco <ricardo.krieg@gmail.com>
2021-02-10 22:27:06 -0300

    design

--- ssesportiva/src/containers/GameContainer.js
+++ ssesportiva/src/containers/GameContainer.js
@@ -10,6 +10,8 @@ import { addOption, removeOption } from '../actions/bet';
 import CurrentBet from '../components/CurrentBet';
 import Loading from '../components/Loading';
 import Error from '../components/Error';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClock } from '@fortawesome/free-solid-svg-icons';
 
 class GameContainer extends React.Component {
   componentDidMount() {
@@ -45,6 +47,18 @@ class GameContainer extends React.Component {
     return this.isSelectedOption(option) ? 'warning' : 'primary';
   }
 
+  getHour(game) {
+    return game.date.split(' ')[1];
+  }
+
+  getQuoteType(quote) {
+    if (quote.type.length > 30) {
+      return <h5 className="text-center m-3">{quote.type}</h5>;
+    } else {
+      return <h4 className="text-center m-3">{quote.type}</h4>;
+    }
+  }
+
   render() {
     const { loading, error, game } = this.props;
 
@@ -52,33 +66,46 @@ class GameContainer extends React.Component {
     if (error) return <Error error={error} />;
 
     return (
-      <div>
+      <>
         <CurrentBet />
 
-        {game.quotes.map((quote, index) => (
-          <Card key={index + 1}>
-            <Card.Header>{quote.type}</Card.Header>
-            <Card.Body>
-              <div>
-                {quote.options.map((option, optionIndex) => (
-                  <div key={optionIndex + 1}>
-                    <div>{option.id}</div>
-                    <div>{option.title}</div>
-                    <Button
-                      variant={this.btnVariant(option)}
-                      onClick={() =>
-                        this.handleOptionClick(game, quote, option)
-                      }
-                    >
-                      {option.quote}
-                    </Button>
-                  </div>
-                ))}
-              </div>
-            </Card.Body>
-          </Card>
-        ))}
-      </div>
+        <div className="px-3 position-relative" style={{ paddingTop: '54px' }}>
+          <h3 className="text-center mt-3">{game.title}</h3>
+          <span
+            className="position-absolute bottom-0 badge bg-secondary"
+            style={{ right: '10px' }}
+          >
+            <FontAwesomeIcon icon={faClock} />
+            <span className="mx-1">{this.getHour(game)}</span>
+          </span>
+        </div>
+
+        <hr />
+
+        <div className="m-3">
+          {game.quotes.map((quote, index) => (
+            <div key={index + 1} className="card shadow">
+              {this.getQuoteType(quote)}
+
+              {quote.options.map((option, optionIndex) => (
+                <div
+                  key={optionIndex + 1}
+                  className="border d-flex justify-content-between align-items-center p-3"
+                >
+                  <h5 className="m-0">{option.title}</h5>
+                  <Button
+                    className="border-white rounded w-25"
+                    variant={this.btnVariant(option)}
+                    onClick={() => this.handleOptionClick(game, quote, option)}
+                  >
+                    {option.quote}
+                  </Button>
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      </>
     );
   }
 }
